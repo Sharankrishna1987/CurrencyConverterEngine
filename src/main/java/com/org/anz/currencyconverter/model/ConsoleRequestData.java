6025048955b9befe9a1be50b7e8668ece931f21a
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:37:07 +0530

    Bug fix

--- src/main/java/com/org/anz/currencyconverter/model/ConsoleRequestData.java
+++ src/main/java/com/org/anz/currencyconverter/model/ConsoleRequestData.java
@@ -52,4 +52,40 @@ public class ConsoleRequestData {
 		return amount;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((base == null) ? 0 : base.hashCode());
+		result = prime * result + ((terms == null) ? 0 : terms.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsoleRequestData other = (ConsoleRequestData) obj;
+		if (base == null) {
+			if (other.base != null)
+				return false;
+		} else if (!base.equals(other.base))
+			return false;
+		if (terms == null) {
+			if (other.terms != null)
+				return false;
+		} else if (!terms.equals(other.terms))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ConsoleRequestData [base=" + base + ", terms=" + terms + "]";
+	}
+	
 }
