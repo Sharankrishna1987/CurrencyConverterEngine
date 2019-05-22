78cb161f2c77861c1a133d6b8e5899692ad6fa76
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:47:38 +0530

    Bug fix

--- src/test/java/com/org/anz/currencyconverter/util/CurrencyCalculatorImplTest.java
+++ src/test/java/com/org/anz/currencyconverter/util/CurrencyCalculatorImplTest.java
@@ -3,7 +3,9 @@ package com.org.anz.currencyconverter.util;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -25,31 +27,13 @@ public class CurrencyCalculatorImplTest {
 	private CurrencyCalculatorImpl currencyCalculatorImpl;
 
 	@Test
-	public void testCalculateConversionRateForDirectFeed() {
+	public void testCalculate() {
 		Map<String, BigDecimal> directFeed = new HashMap<>();
 		directFeed.put("AUDUSD", new BigDecimal("0.8371"));
+		List<String> calculationSteps = Arrays.asList("AUDUSD");
 		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("USD").amount(new BigDecimal("100.00")).build();
 		when(currencyConverterSpreadSheetReader.readDirectFeed()).thenReturn(directFeed);
-		Assert.assertEquals(new BigDecimal("83.71"), currencyCalculatorImpl.calculateConversionRateForDirectFeed(consoleRequestData));
+		Assert.assertEquals(new BigDecimal("83.71"), currencyCalculatorImpl.calculate(calculationSteps, consoleRequestData));
 	}
 	
-	@Test
-	public void testCalculateConversionRateForInverted() {
-		Map<String, BigDecimal> directFeed = new HashMap<>();
-		directFeed.put("USDJPY", new BigDecimal("119.95"));
-		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("JPY").terms("USD").amount(new BigDecimal("100")).build();
-		when(currencyConverterSpreadSheetReader.readDirectFeed()).thenReturn(directFeed);
-		Assert.assertEquals(new BigDecimal("0.83"), currencyCalculatorImpl.calculateConversionRateForInverted(consoleRequestData));
-	}
-	
-	@Test
-	public void testCalculateConversionRateForCrossViaCurrency() {
-		Map<String, BigDecimal> directFeed = new HashMap<>();
-		directFeed.put("AUDUSD", new BigDecimal("0.8371"));
-		directFeed.put("USDJPY", new BigDecimal("119.95"));
-		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("JPY").amount(new BigDecimal("1")).build();
-		when(currencyConverterSpreadSheetReader.readDirectFeed()).thenReturn(directFeed);
-		Assert.assertEquals(new BigDecimal("100"), currencyCalculatorImpl.calculateConversionRateForCrossViaCurrency(consoleRequestData, "USD"));
-	}
-
 }
