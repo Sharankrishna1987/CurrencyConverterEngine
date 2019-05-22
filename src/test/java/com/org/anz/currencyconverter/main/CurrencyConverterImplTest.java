98035607d4f0e85e64e4f910d143e5c67e7405e5
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:46:18 +0530

    Bug fix

--- src/test/java/com/org/anz/currencyconverter/main/CurrencyConverterImplTest.java
+++ src/test/java/com/org/anz/currencyconverter/main/CurrencyConverterImplTest.java
@@ -3,6 +3,7 @@ package com.org.anz.currencyconverter.main;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Rule;
@@ -38,48 +39,29 @@ public class CurrencyConverterImplTest {
 	private CurrencyConverterImpl currencyConverterImpl;
 	
 	@Test
-	public void testConvertForUnity() {
-		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("AUD").amount(new BigDecimal("100.00")).build();
-		when(consoleInputParser.parse("AUD 100.00 in AUD")).thenReturn(consoleRequestData);
-		when(crossViaMatrixDataProvider.getCalculationModel("AUDAUD")).thenReturn("1:1");
-		Assert.assertEquals("AUD 100.00 = AUD 100.00", currencyConverterImpl.convert("AUD 100.00 in AUD"));
-	}
-	
-	@Test
-	public void testConvertForDirectFeed() {
+	public void testConvert() {
 		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("USD").amount(new BigDecimal("100.00")).build();
 		when(consoleInputParser.parse("AUD 100.00 in USD")).thenReturn(consoleRequestData);
-		when(crossViaMatrixDataProvider.getCalculationModel("AUDUSD")).thenReturn("D");
-		when(currencyCalculator.calculateConversionRateForDirectFeed(consoleRequestData)).thenReturn(new BigDecimal("83.71"));
+		when(crossViaMatrixDataProvider.getCalculationSteps(consoleRequestData)).thenReturn(Arrays.asList("AUDUSD"));
+		when(currencyCalculator.calculate(Arrays.asList("AUDUSD"), consoleRequestData)).thenReturn(new BigDecimal("83.71"));
 		Assert.assertEquals("AUD 100.00 = USD 83.71", currencyConverterImpl.convert("AUD 100.00 in USD"));
 	}
 	
 	@Test
-	public void testConvertForInverted() {
-		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("JPY").terms("USD").amount(new BigDecimal("100")).build();
-		when(consoleInputParser.parse("JPY 100 in USD")).thenReturn(consoleRequestData);
-		when(crossViaMatrixDataProvider.getCalculationModel("JPYUSD")).thenReturn("Inv");
-		when(currencyCalculator.calculateConversionRateForInverted(consoleRequestData)).thenReturn(new BigDecimal("0.83"));
-		Assert.assertEquals("JPY 100 = USD 0.83", currencyConverterImpl.convert("JPY 100 in USD"));
+	public void testConvertForUnity() {
+		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("AUD").amount(new BigDecimal("100.00")).build();
+		when(consoleInputParser.parse("AUD 100.00 in AUD")).thenReturn(consoleRequestData);
+		Assert.assertEquals("AUD 100.00 = AUD 100.00", currencyConverterImpl.convert("AUD 100.00 in AUD"));
 	}
 	
 	@Test
-	public void testConvertForCrossCurrency() {
-		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("DKK").amount(new BigDecimal("100.00")).build();
-		when(consoleInputParser.parse("AUD 100.00 in DKK")).thenReturn(consoleRequestData);
-		when(crossViaMatrixDataProvider.getCalculationModel("AUDDKK")).thenReturn("EUR");
-		when(currencyCalculator.calculateConversionRateForCrossViaCurrency(consoleRequestData, "EUR")).thenReturn(new BigDecimal("505.76"));
-		Assert.assertEquals("AUD 100.00 = DKK 505.76", currencyConverterImpl.convert("AUD 100.00 in DKK"));
-	}
-	
-	@Test
-	public void testConvertForInvalidCalculationModel() {
+	public void testConvertForIncorrectCurrencyPair() {
 		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("KRW").terms("FJD").amount(new BigDecimal("1000.00")).build();
 		when(consoleInputParser.parse("KRW 1000.00 in FJD")).thenReturn(consoleRequestData);
-		when(crossViaMatrixDataProvider.getCalculationModel("KRWFJD")).thenReturn(null);
+		when(crossViaMatrixDataProvider.getCalculationSteps(consoleRequestData)).thenReturn(null);
 		expectedException.expect(ValidationException.class);
 		expectedException.expectMessage("Unable to find rate for KRW/FJD");
 		currencyConverterImpl.convert("KRW 1000.00 in FJD");
 	}
-
+	
 }
