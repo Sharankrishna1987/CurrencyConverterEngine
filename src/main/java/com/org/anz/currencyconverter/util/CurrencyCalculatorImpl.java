36e79e3fd43670741f5cc205df2412f23c6d3dcf
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:39:19 +0530

    Bug fix

--- src/main/java/com/org/anz/currencyconverter/util/CurrencyCalculatorImpl.java
+++ src/main/java/com/org/anz/currencyconverter/util/CurrencyCalculatorImpl.java
@@ -2,6 +2,7 @@ package com.org.anz.currencyconverter.util;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,50 +17,22 @@ public class CurrencyCalculatorImpl implements CurrencyCalculator {
 	private CurrencyConverterSpreadSheetReader currencyConverterSpreadSheetReader;
 	
 	@Override
-	public BigDecimal calculateConversionRateForDirectFeed(ConsoleRequestData consoleRequestData) {
-		
+	public BigDecimal calculate(List<String> calculationSteps, ConsoleRequestData consoleRequestData) {
+
 		Map<String, BigDecimal> directFeed = currencyConverterSpreadSheetReader.readDirectFeed();
-		String inputCurrencyPair = consoleRequestData.getBase().concat(consoleRequestData.getTerms());
+		BigDecimal totalAmount = BigDecimal.ONE;
+
+		for (String calculationStep : calculationSteps) {
+			totalAmount = totalAmount.multiply(directFeed.get(calculationStep));
+		}
 
 		Currency currency = Currency.getInstance(consoleRequestData.getTerms());
 
-		BigDecimal convertedValue = consoleRequestData.getAmount().multiply(directFeed.get(inputCurrencyPair));
-		convertedValue = convertedValue.setScale(currency.getDefaultFractionDigits(), BigDecimal.ROUND_DOWN);
+		BigDecimal convertedAmount = consoleRequestData.getAmount().multiply(totalAmount);
+		convertedAmount = convertedAmount.setScale(currency.getDefaultFractionDigits(), BigDecimal.ROUND_DOWN);
 
-		return convertedValue;
-
-	}
-
-	@Override
-	public BigDecimal calculateConversionRateForInverted(ConsoleRequestData consoleRequestData) {
-		
-		Map<String, BigDecimal> directFeed = currencyConverterSpreadSheetReader.readDirectFeed();
-		String inputCurrencyPair = consoleRequestData.getTerms().concat(consoleRequestData.getBase());
-
-		Currency currency = Currency.getInstance(consoleRequestData.getTerms());
-
-		BigDecimal convertedValue = consoleRequestData.getAmount().divide(directFeed.get(inputCurrencyPair), currency.getDefaultFractionDigits(), BigDecimal.ROUND_DOWN);
-
-		return convertedValue;
+		return convertedAmount;
 
 	}
 
-	@Override
-	public BigDecimal calculateConversionRateForCrossViaCurrency(ConsoleRequestData consoleRequestData, String crossCurrency) {
-
-		Map<String, BigDecimal> directFeed = currencyConverterSpreadSheetReader.readDirectFeed();
-		
-		String inputCurrencyPairForBase = consoleRequestData.getBase().concat(crossCurrency);
-		BigDecimal directFeedCrossViaBaseValue = consoleRequestData.getAmount().multiply(directFeed.get(inputCurrencyPairForBase));
-		
-		String inputCurrencyPairForTerm = crossCurrency.concat(consoleRequestData.getTerms());
-		BigDecimal directFeedCrossViaTermValue = directFeedCrossViaBaseValue.multiply(directFeed.get(inputCurrencyPairForTerm));
-		
-		Currency currency = Currency.getInstance(consoleRequestData.getTerms());
-		directFeedCrossViaTermValue = directFeedCrossViaTermValue.setScale(currency.getDefaultFractionDigits(), BigDecimal.ROUND_DOWN);
-				
-		return directFeedCrossViaTermValue;
-
-	}
-	
 }
