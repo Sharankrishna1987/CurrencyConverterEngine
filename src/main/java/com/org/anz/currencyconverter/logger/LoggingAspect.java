e59df0a9a698234da201fbd7d5abf7d6eb174319
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:34:03 +0530

    Bug fix

--- src/main/java/com/org/anz/currencyconverter/logger/LoggingAspect.java
+++ src/main/java/com/org/anz/currencyconverter/logger/LoggingAspect.java
@@ -2,6 +2,8 @@ package com.org.anz.currencyconverter.logger;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -11,17 +13,31 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
-	
+
 	@Before("execution(* com.org.anz..currencyconverter..*(..))")
 	public void logBefore(JoinPoint joinPoint) {
-		LOGGER.info(String.format("Inside method: %s() of class: %s", joinPoint.getSignature().getName(), joinPoint.getSignature().getDeclaringTypeName()));
+		LOGGER.info(String.format("Inside method: %s() of class: %s", joinPoint.getSignature().getName(),
+				joinPoint.getSignature().getDeclaringTypeName()));
+	}
+
+	@AfterReturning(pointcut = "execution(* com.org.anz..currencyconverter..*(..))", returning = "result")
+	public void logAfterReturning(JoinPoint joinPoint, Object result) {
+		LOGGER.info(String.format("Method: %s() of class: %s returned value: %s", joinPoint.getSignature().getName(),
+				joinPoint.getSignature().getDeclaringTypeName(), result));
 	}
 
 	@After("execution(* com.org.anz..currencyconverter..*(..))")
 	public void logAfter(JoinPoint joinPoint) {
-		LOGGER.info(String.format("Exiting method: %s() of class: %s", joinPoint.getSignature().getName(), joinPoint.getSignature().getDeclaringTypeName()));
+		LOGGER.info(String.format("Exiting method: %s() of class: %s", joinPoint.getSignature().getName(),
+				joinPoint.getSignature().getDeclaringTypeName()));
 	}
-	
+
+	@AfterThrowing(pointcut = "execution(* com.org.anz..currencyconverter..*(..))", throwing = "error")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+		LOGGER.info(String.format("Method: %s() of class: %s threw: %s", joinPoint.getSignature().getName(),
+				joinPoint.getSignature().getDeclaringTypeName(), error));
+	}
+
 }
