e197bd4d14d388f7db81db0358d246f292d706eb
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:33:08 +0530

    Bug fix

--- src/main/java/com/org/anz/currencyconverter/constant/ApplicationConstant.java
+++ src/main/java/com/org/anz/currencyconverter/constant/ApplicationConstant.java
@@ -1,9 +1,12 @@
 package com.org.anz.currencyconverter.constant;
 
 public class ApplicationConstant {
-	
+
 	public static final String APP_ERROR_MESSAGE = "Application Exception";
 	public static final String CROSS_VIA_MATRIX_FILE_LOCATION = "templates/cross-via-matrix.xlsx";
 	public static final String DIRECT_FEED_FILE_LOCATION = "templates/direct-feed.xlsx";
+	public static final String DIRECT_FEED_VALUE = "D";
+	public static final String INVERTED_VALUE = "Inv";
+	public static final String UNITY_VALUE = "1:1";
 
 }
