4fbf77cbd2346979329db57e215bb381912cf28b
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 11:16:53 +0530

    Code refactoring

--- src/main/java/com/org/anz/currencyconverter/wrapper/ApplicationWrapper.java
+++ src/main/java/com/org/anz/currencyconverter/wrapper/ApplicationWrapper.java
@@ -3,6 +3,7 @@ package com.org.anz.currencyconverter.wrapper;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -11,5 +12,7 @@ public interface ApplicationWrapper {
 	InputStream createInputStream(String fileLocation) throws FileNotFoundException;
 
 	XSSFWorkbook createXSSFWorkbook(InputStream inputStream) throws IOException;
+
+	Scanner getScanner();
 
 }
