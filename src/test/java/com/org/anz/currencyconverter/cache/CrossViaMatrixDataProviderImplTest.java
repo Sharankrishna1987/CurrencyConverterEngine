377f800e4fea1f6626c87fb82ba6b5027baa2e3b
Sharankrishna1987 <50475485+Sharankrishna1987@users.noreply.github.com>
2019-05-22 10:45:21 +0530

    Bug fix

--- src/test/java/com/org/anz/currencyconverter/cache/CrossViaMatrixDataProviderImplTest.java
+++ src/test/java/com/org/anz/currencyconverter/cache/CrossViaMatrixDataProviderImplTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.org.anz.currencyconverter.model.ConsoleRequestData;
 import com.org.anz.currencyconverter.model.CrossViaMatrixData;
 import com.org.anz.currencyconverter.util.CurrencyConverterSpreadSheetReader;
 
@@ -25,11 +26,40 @@ public class CrossViaMatrixDataProviderImplTest {
 	private CrossViaMatrixDataProviderImpl crossViaMatrixDataProviderImpl;
 	
 	@Test
-	public void testGetCalculationModel() {
-		List<CrossViaMatrixData> crossViaMatrixData = Arrays.asList(new CrossViaMatrixData("AUD", "USD", "Inv"));
+	public void testGetCalculationStepsForDirect() {
+		List<CrossViaMatrixData> crossViaMatrixData = Arrays.asList(new CrossViaMatrixData("AUD", "USD", "D"));
 		when(currencyConverterSpreadSheetReader.readCrossViaMatrixData()).thenReturn(crossViaMatrixData);
 		crossViaMatrixDataProviderImpl.readCrossViaMatrixData();
-		Assert.assertEquals("Inv", crossViaMatrixDataProviderImpl.getCalculationModel("AUDUSD"));
+		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("USD").build();
+		Assert.assertEquals(Arrays.asList("AUDUSD"), crossViaMatrixDataProviderImpl.getCalculationSteps(consoleRequestData));
+	}
+	
+	@Test
+	public void testGetCalculationStepsForInverted() {
+		List<CrossViaMatrixData> crossViaMatrixData = Arrays.asList(new CrossViaMatrixData("JPY", "USD", "Inv"));
+		when(currencyConverterSpreadSheetReader.readCrossViaMatrixData()).thenReturn(crossViaMatrixData);
+		crossViaMatrixDataProviderImpl.readCrossViaMatrixData();
+		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("JPY").terms("USD").build();
+		Assert.assertEquals(Arrays.asList("JPYUSD"), crossViaMatrixDataProviderImpl.getCalculationSteps(consoleRequestData));
+	}
+	
+	@Test
+	public void testGetCalculationStepsForUnity() {
+		List<CrossViaMatrixData> crossViaMatrixData = Arrays.asList(new CrossViaMatrixData("AUD", "AUD", "1:1"));
+		when(currencyConverterSpreadSheetReader.readCrossViaMatrixData()).thenReturn(crossViaMatrixData);
+		crossViaMatrixDataProviderImpl.readCrossViaMatrixData();
+		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("AUD").build();
+		Assert.assertEquals(null, crossViaMatrixDataProviderImpl.getCalculationSteps(consoleRequestData));
+	}
+	
+	@Test
+	public void testGetCalculationStepsForCrossCurrency() {
+		List<CrossViaMatrixData> crossViaMatrixData = Arrays.asList(new CrossViaMatrixData("AUD", "DKK", "USD"), new CrossViaMatrixData("AUD", "USD", "D"),
+				new CrossViaMatrixData("USD", "EUR", "Inv"), new CrossViaMatrixData("EUR", "DKK", "D"), new CrossViaMatrixData("USD", "DKK", "EUR"));
+		when(currencyConverterSpreadSheetReader.readCrossViaMatrixData()).thenReturn(crossViaMatrixData);
+		crossViaMatrixDataProviderImpl.readCrossViaMatrixData();
+		ConsoleRequestData consoleRequestData = new ConsoleRequestData.Builder().base("AUD").terms("DKK").build();
+		Assert.assertEquals(Arrays.asList("AUDUSD", "USDEUR", "EURDKK"), crossViaMatrixDataProviderImpl.getCalculationSteps(consoleRequestData));
 	}
 
 }
